f0350169397bb8f6b67bd3e3f016cc8649d37793
Srujan Alikanti <Srujan@DiagonalMatrix.com>
2026-02-27 07:27:17 +0000

    remove comments

--- app/generation.py
+++ app/generation.py
@@ -79,22 +79,3 @@ if __name__ == "__main__":
         print("the score is --> ", ques_score[1])
 
     
-
-
-
-# class AnswerGenerator():
-#     def __init__(self, provider: str, model:str, temperature: float, max_token: int):
-#         pass
-
-#     def generate(self, question: str, contect: str) -> str:
-#         """uses prompt builder from prompts.py
-#         calls OpenAI chat client from llm.py"""
-#         pass
-
-#     def generate_with_sources(self, question: str, context: str, docs: list[Document]) -> dict:
-#         return {"answer":"",
-#                 "sources":""}
-    
-#     def post_process(self, answer: str) -> str:
-#         """trims, sanity checks, handles “I don’t know” style outputs"""
-#         pass
